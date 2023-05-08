5c3ed6161fc9f4dfda96ef6a6149f1f01a54e971
WelchJ <jack_weq@icloud.com>
2023-05-08 11:28:43 -0400

    rendered as md and added a button for copy to clipboard

--- script.js
+++ script.js
@@ -5,13 +5,25 @@ function displayDiff() {
     var output = "";
     for (var i = 0; i < diff.length; i++) {
       if (diff[i].added) {
-        output += "*_" + DOMPurify.sanitize(diff[i].value) + "_*";
+        output += " *" + DOMPurify.sanitize(diff[i].value) + "*";
       } else if (diff[i].removed) {
         output += "~~" + DOMPurify.sanitize(diff[i].value) + "~~";
       } else {
         output += DOMPurify.sanitize(diff[i].value);
       }
     }
-    document.getElementById("output").value = output;
+    var md = window.markdownit();
+    document.getElementById("output").innerHTML = md.render(output);
+    // document.getElementById("output").value = output;
+    
   }
   
+  function copyToClipboard() {
+    const outputElement = document.getElementById("output");
+    const range = document.createRange();
+    range.selectNode(outputElement);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();
+  }
