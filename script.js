5e0b9f9361125bce3d8d1ad82a0efb38b68a2426
Jack Wei <jack_weq@icloud.com>
2023-05-08 19:22:54 -0400

    working version, try to develop an algorithm to process large chunk of deletions

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
+// function displayDiff() {
+//   var before = document.getElementById("before").value;
+//   var after = document.getElementById("after").value;
+//   var diff = diffWords(before, after);
+//   var output = "";
+//   for (var i = 0; i < diff.length; i++) {
+//     if (diff[i].added) {
+//       output += "*" + DOMPurify.sanitize(diff[i].value) + "* ";
+//     } else if (diff[i].removed) {
+//       output += "~~" + DOMPurify.sanitize(diff[i].value) + "~~ ";
+//     } else {
+//       output += DOMPurify.sanitize(diff[i].value);
+//     }
+//   }
+//   var md = window.markdownit();
+//   document.getElementById("output").innerHTML = md.render(output);
+// }
+
+  // function copyToClipboard() {
+  //   const outputElement = document.getElementById("output");
+  //   const range = document.createRange();
+  //   range.selectNode(outputElement);
+  //   window.getSelection().removeAllRanges();
+  //   window.getSelection().addRange(range);
+  //   document.execCommand("copy");
+  //   window.getSelection().removeAllRanges();
+  // }
+
 function displayDiff() {
-    var before = document.getElementById("before").value;
-    var after = document.getElementById("after").value;
-    var diff = Diff.diffWords(before, after);
-    var output = "";
-    for (var i = 0; i < diff.length; i++) {
-      if (diff[i].added) {
-        output += " *" + DOMPurify.sanitize(diff[i].value) + "*";
-      } else if (diff[i].removed) {
-        output += "~~" + DOMPurify.sanitize(diff[i].value) + "~~";
-      } else {
-        output += DOMPurify.sanitize(diff[i].value);
-      }
+  var before = document.getElementById("before").value;
+  var after = document.getElementById("after").value;
+  // const diff = Diff.diffWords(before, after);
+  const diff = Diff.diffWordsWithSpace(before, after);
+
+  let html = '<span>';
+  for (let i = 0; i < diff.length; i++) {
+    if (diff[i].added) {
+      html += '<i>' + diff[i].value + '</i><span> </span>';
+    } else if (diff[i].removed) {
+      html += '<s>' + diff[i].value + '</s><span> </span>';
+    } else {
+      html += '' + diff[i].value + '';
     }
-    var md = window.markdownit();
-    document.getElementById("output").innerHTML = md.render(output);
-    // document.getElementById("output").value = output;
-    
   }
-  
+  html += '</span>';
+  document.getElementById("output").innerHTML = html;
+}
+
   function copyToClipboard() {
     const outputElement = document.getElementById("output");
     const range = document.createRange();
